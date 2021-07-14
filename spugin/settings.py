56d0aabc2fb54e30af754a3e9c4de781998d1f3b
Tommy Dougiamas <tom@digitalnook.net>
2021-07-14 20:15:29 +0800

    fix config file remnants

--- spugin/settings.py
+++ spugin/settings.py
@@ -1,8 +1,6 @@
-import appdirs
 from pathlib import Path
 
 # Static values
 VERSION = 0.1
 API_URL = "http://api.spiget.org/v2"
 USER_AGENT = f"Spugin/{VERSION}"
-CONFIG_FILE = Path(f"{appdirs.user_config_dir(appname='spugin')}/spugin.yml").__str__()
