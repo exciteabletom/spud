ecd7a8b594a1f7475ac8200ef3579f37bd19e52e
exciteabletom <tom@digitalnook.net>
2021-08-31 10:04:10 +0800

    bump version

--- spud/settings.py
+++ spud/settings.py
@@ -8,7 +8,7 @@ Variables:
     METADATA_FILENAME - The filename for metadata files saved in jars
 """
 # Static values
-VERSION = "1.1.4"
+VERSION = "1.2"
 BASE_API_URL = "https://api.spiget.org/v2"
 USER_AGENT = f"Spud/{VERSION}"
 METADATA_FILENAME = ".spud_meta.json"
